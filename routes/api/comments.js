91a44106ba39f0e979f2947c8a15f4396409d4cd
beijingjack <lijingyu951126@gmail.com>
2019-06-03 22:05:07 +0800

    Completed create & update comment

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -8,7 +8,7 @@ const User = require("../../models/User");
 const Movie = require("../../models/Movie");
 
 // @route    POST api/comments
-// @desc     Create a comment about a movie
+// @desc     Create or update a comment about a movie
 // @access   Private
 router.post(
   "/",
@@ -28,134 +28,69 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-    let movie;
+    const { movieId, text, movieRating } = req.body;
     try {
-      const currComment = await Comment.findOne({
+      const user = await User.findById(req.user.id).select("-password");
+      // Build comment object
+      const commentFields = {};
+      commentFields.username = user.username;
+      commentFields.user = req.user.id;
+      commentFields.movieId = movieId;
+      commentFields.text = text;
+      if (movieRating) {
+        commentFields.movieRating = movieRating;
+      }
+
+      let comment = await Comment.findOne({
         user: req.user.id,
         movieId: req.body.movieId
       });
-      if (currComment) {
-        return res.status(400).json({
-          errors: [{ msg: "Each user can only make one comment about a movie" }]
-        });
+      let prevMovieRating;
+      if (comment) {
+        comment.movieRating && (prevMovieRating = comment.movieRating);
+        comment = await Comment.findOneAndUpdate(
+          {
+            user: req.user.id,
+            movieId: movieId
+          },
+          { $set: commentFields },
+          { new: true }
+        );
+      } else {
+        comment = new Comment(commentFields);
+        await comment.save();
       }
-
-      const user = await User.findById(req.user.id).select("-password");
-      const newComment = {
-        text: req.body.text,
-        movieId: req.body.movieId,
-        username: user.username,
-        user: req.user.id,
-        movieRating: req.body.movieRating
-      };
-
-      const comment = new Comment(newComment);
-
-      await comment.save();
-
       // Update average rating of the movie
-
-      const tmp = await Movie.findOne({ movieId: req.body.movieId });
-      if (!tmp && req.body.movieRating) {
+      let movie = await Movie.findOne({ movieId: movieId });
+      if (!movie && movieRating) {
         movie = new Movie({
-          movieId: req.body.movieId,
-          avgRating: req.body.movieRating,
+          movieId: movieId,
+          avgRating: movieRating,
           totalVotes: 1
         });
         await movie.save();
         return res.json(comment);
       }
-      if (tmp && req.body.movieRating) {
-        const prevTotal = tmp.totalVotes;
-        const currTotal = prevTotal + 1;
-        const currAvg =
-          (prevTotal * tmp.avgRating + req.body.movieRating) / currTotal;
-        movie = await Movie.findOneAndUpdate(
-          { movieId: req.body.movieId },
+      if (movie && movieRating) {
+        let prevTotal = movie.totalVotes;
+        let currTotal;
+        let currAvg;
+        if (prevMovieRating) {
+          currTotal = prevTotal;
+          currAvg =
+            (prevTotal * movie.avgRating - prevMovieRating + movieRating) /
+            currTotal;
+        } else {
+          currTotal = prevTotal + 1;
+          currAvg = (prevTotal * movie.avgRating + movieRating) / currTotal;
+        }
+        await Movie.findOneAndUpdate(
+          { movieId: movieId },
           { $set: { avgRating: currAvg, totalVotes: currTotal } }
         );
         return res.json(comment);
       }
-      res.json(comment);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  }
-);
 
-// @todo: Edit a comment
-// @route    PUT api/comments/:id
-// @desc     Edit user's own comment about a movie
-// @access   Private
-router.post(
-  "/",
-  [
-    auth,
-    [
-      check("movieId", "MovieID is required")
-        .not()
-        .isEmpty(),
-      check("text", "Text is required")
-        .not()
-        .isEmpty()
-    ]
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    let movie;
-    try {
-      const currComment = await Comment.findOne({
-        user: req.user.id,
-        movieId: req.body.movieId
-      });
-      if (currComment) {
-        return res.status(400).json({
-          errors: [{ msg: "Each user can only make one comment about a movie" }]
-        });
-      }
-
-      const user = await User.findById(req.user.id).select("-password");
-      const newComment = {
-        text: req.body.text,
-        movieId: req.body.movieId,
-        username: user.username,
-        user: req.user.id,
-        movieRating: req.body.movieRating
-      };
-
-      const comment = new Comment(newComment);
-
-      await comment.save();
-
-      // Update average rating of the movie
-
-      const tmp = await Movie.findOne({ movieId: req.body.movieId });
-      if (!tmp && req.body.movieRating) {
-        movie = new Movie({
-          movieId: req.body.movieId,
-          avgRating: req.body.movieRating,
-          totalVotes: 1
-        });
-        await movie.save();
-        return res.json(comment);
-      }
-      if (tmp && req.body.movieRating) {
-        const prevTotal = tmp.totalVotes;
-        const currTotal = prevTotal + 1;
-        const currAvg =
-          (prevTotal * tmp.avgRating + req.body.movieRating) / currTotal;
-        movie = await Movie.findOneAndUpdate(
-          { movieId: req.body.movieId },
-          { $set: { avgRating: currAvg, totalVotes: currTotal } }
-        );
-        return res.json(comment);
-      }
       res.json(comment);
     } catch (err) {
       console.error(err.message);
